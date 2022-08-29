5133ad26aa8effcea22d1ffa515fcbb302611bdd
jun108059 <youngjun108059@gmail.com>
2022-08-30 00:13:12 +0900

    ♻️ 리팩토링 9. 객체 통째로 넘기기

---
+++ src/main/java/study/yjpark/_03_long_function/_09_preserve_whole_object/StudyDashboardBefore.java
@@ -0,0 +1,134 @@
+package study.yjpark._03_long_function._09_preserve_whole_object;
+
+import org.kohsuke.github.GHIssue;
+import org.kohsuke.github.GHIssueComment;
+import org.kohsuke.github.GHRepository;
+import org.kohsuke.github.GitHub;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * 함수의 파라미터가 객체(whole object)에 의존해도 좋은가 고민해봐야 함
+ */
+public class StudyDashboardBefore {
+
+    private final int totalNumberOfEvents;
+
+    public StudyDashboardBefore(int totalNumberOfEvents) {
+        this.totalNumberOfEvents = totalNumberOfEvents;
+    }
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        StudyDashboardBefore studyDashboard = new StudyDashboardBefore(15);
+        studyDashboard.print();
+    }
+
+    private void print() throws IOException, InterruptedException {
+        GitHub gitHub = GitHub.connect();
+        GHRepository repository = gitHub.getRepository("whiteship/live-study");
+        List<Participant> participants = new CopyOnWriteArrayList<>();
+
+        ExecutorService service = Executors.newFixedThreadPool(8);
+        CountDownLatch latch = new CountDownLatch(totalNumberOfEvents);
+
+        // 15개의 이슈에 댓글이 있는지 확인
+        for (int index = 1 ; index <= totalNumberOfEvents ; index++) {
+            int eventId = index;
+            service.execute(() -> {
+                try {
+                    GHIssue issue = repository.getIssue(eventId);
+                    List<GHIssueComment> comments = issue.getComments();
+
+                    for (GHIssueComment comment : comments) {
+                        String username = comment.getUserName();
+                        boolean isNewUser = participants.stream().noneMatch(p -> p.username().equals(username));
+                        Participant participant;
+                        if (isNewUser) {
+                            participant = new Participant(username);
+                            participants.add(participant);
+                        } else {
+                            participant = participants.stream().filter(p -> p.username().equals(username)).findFirst().orElseThrow();
+                        }
+
+                        participant.setHomeworkDone(eventId);
+                    }
+
+                    latch.countDown();
+                } catch (IOException e) {
+                    throw new IllegalArgumentException(e);
+                }
+            });
+        }
+
+        latch.await();
+        service.shutdown();
+
+        // Markdown 파일로 출력
+        try (FileWriter fileWriter = new FileWriter("participants.md");
+             PrintWriter writer = new PrintWriter(fileWriter)) {
+            participants.sort(Comparator.comparing(Participant::username));
+
+            writer.print(header(participants.size()));
+
+            participants.forEach(p -> {
+                String markdownForHomework = getMarkdownForParticipant(p.username(), p.homework());
+                writer.print(markdownForHomework);
+            });
+        }
+    }
+
+    // Query(질의 : 값을 계산하는 함수)로 추출
+    private double getRate(Map<Integer, Boolean> homework) {
+        long count = homework.values().stream()
+                .filter(v -> v == true)
+                .count();
+        return (double) (count * 100 / this.totalNumberOfEvents);
+    }
+
+    // Query(질의 : 값을 계산하는 함수)로 추출
+    private String getMarkdownForParticipant(String username, Map<Integer, Boolean> homework) {
+        return String.format("| %s %s | %.2f%% |\n", username, checkMark(homework, this.totalNumberOfEvents), getRate(homework));
+    }
+
+    /**
+     * | 참여자 (420) | 1주차 | 2주차 | 3주차 | 참석율 |
+     * | --- | --- | --- | --- | --- |
+     */
+    private String header(int totalNumberOfParticipants) {
+        StringBuilder header = new StringBuilder(String.format("| 참여자 (%d) |", totalNumberOfParticipants));
+
+        for (int index = 1; index <= this.totalNumberOfEvents; index++) {
+            header.append(String.format(" %d주차 |", index));
+        }
+        header.append(" 참석율 |\n");
+
+        header.append("| --- ".repeat(Math.max(0, this.totalNumberOfEvents + 2)));
+        header.append("|\n");
+
+        return header.toString();
+    }
+
+    /**
+     * |:white_check_mark:|:white_check_mark:|:white_check_mark:|:x:|
+     */
+    private String checkMark(Map<Integer, Boolean> homework, int totalEvents) {
+        StringBuilder line = new StringBuilder();
+        for (int i = 1 ; i <= totalEvents; i++) {
+            if(homework.containsKey(i) && homework.get(i)) {
+                line.append("|:white_check_mark:");
+            } else {
+                line.append("|:x:");
+            }
+        }
+        return line.toString();
+    }
+}
